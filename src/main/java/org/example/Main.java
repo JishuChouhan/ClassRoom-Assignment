56522ef3804de9066810770424338f46b2deaabc
JishuChouhan <119945818+JishuChouhan@users.noreply.github.com>
2023-09-03 22:40:40 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,7 +11,7 @@ import java.net.URL;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        String urlString = "https://api.chucknorris.io/jokes/random";
+        String urlString = "https://official-joke-api.appspot.com/random_joke";
 
         URL url = new URL(urlString);
 
